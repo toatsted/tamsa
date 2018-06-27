38fded1643b1428f4c62dae03f4007e8a06e0a95
Sam <samueljacksonhicks@gmail.com>
2018-06-27 12:39:03 -0500

    add testimonials

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import Masthead from './components/Masthead';
 import IconGrid from './components/IconGrid';
 import ImgShowcase from './components/ImgShowcase';
+import Testimonials from './components/Testimonials';
 import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
 
@@ -15,6 +16,7 @@ class App extends Component {
         <Masthead />
         <IconGrid />
         <ImgShowcase />
+        <Testimonials />
         <CallToAction />
         <Footer />
       </div>
