783ea086df75ebea9ac85c2122c968c3c26a36c8
Sam <samueljacksonhicks@gmail.com>
2018-10-22 15:50:07 -0500

    heroku bugfixes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const ip = require("ip");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
